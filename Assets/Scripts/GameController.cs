5e7a3ee5d7003394782c55487160ccc8265ad113
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2023-07-11 14:19:50 +0300

    Might change later

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -101,7 +101,6 @@ public class GameController : MonoBehaviour
         this.ballController.Stop();
         this.ball.transform.position = this.startingPosition;
         this.ballController.Go();
-
     }
     
 }
